c5cd72239bbb16fae538ebb0c9a8013506779e1d
Gökay Gültekin <58526116+naphteine@users.noreply.github.com>
2023-09-03 13:46:30 +0300

    Problem was on my setup lol

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -5,12 +5,7 @@ import { AuthProvider } from "@/context/AuthProvider";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider
-      storageKey="theme"
-      defaultTheme="system"
-      attribute="class"
-      enableSystem
-    >
+    <ThemeProvider defaultTheme="system" attribute="class" enableSystem>
       <AuthProvider>{children}</AuthProvider>
     </ThemeProvider>
   );
